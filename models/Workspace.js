af92f7f7d75d32d9fe8ba90455c81d7c2ceca3b8
Gerard Guasch <haldawe@hotmail.com>
2023-03-28 19:30:02 +0200

    created Workspace model

---
+++ models/Workspace.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const workspaceSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Workspace name required"]
+  },
+  founder: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: [true, "Founder required"]
+  },
+  acronym: {
+    type: String
+  },
+  profilePicture: {
+    type: String
+  },
+  members: [{
+    type: mongoose.Types.ObjectId,
+    ref: "User"
+  }],
+  admins: [{
+    type: mongoose.Types.ObjectId,
+    ref: "User"
+  }],
+  info: {
+    type: String
+  }
+},
+  {
+    timestamps: true
+  });
+
+module.exports = model("Workspace", workspaceSchema);
---
+++ routes/workspace.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const Workspace = require("../models/Workspace");
+
+
+
+module.exports = router;
